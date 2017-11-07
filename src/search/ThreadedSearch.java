b880235c1e98bf044a2cf54f9f1900bb23e99095
Danish Malik <malik083@morris.umn.edu>
2017-11-07 17:49:20 -0600

    Write 'ThreadedSearch.java'.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -21,51 +21,54 @@ public class ThreadedSearch<T> implements Runnable {
     this.answer=answer;
   }
 
-  /**
-  * Searches `list` in parallel using `numThreads` threads.
-  *
-  * You can assume that the list size is divisible by `numThreads`
-  */
-  public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
-    /*
-    * First construct an instance of the `Answer` inner class. This will
-    * be how the threads you're about to create will "communicate". They
-    * will all have access to this one shared instance of `Answer`, where
-    * they can update the `answer` field inside that instance.
-    *
-    * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-    * using the 5 argument constructor for the class. You'll hand each of
-    * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `begin` and `end` values, which you'll
-    * use to give each instance the "job" of searching a different segment
-    * of the list. If, for example, the list has length 100 and you have
-    * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-    * [50, 75), and [75, 100) as their sections to search.
-    *
-    * You then construct `numThreads`, each of which is given a different
-    * instance of this class as its `Runnable`. Then start each of those
-    * threads, wait for them to all terminate, and then return the answer
-    * in the shared `Answer` instance.
-    */
+  public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws{
+    int section = list.size()/numThreads;
+    Thread[] thread = new Thread[numThreads];
+    ArrayList<Answer> result = new ArrayList<Answer>();
+
+    begin = 0;
+    end = section;
+
+    for(int i = 0; i < numThreads; ++i){
+       results.add(new Answer());
+       thread[i] = new Thread(new ThreadedSearch<T>(target,list,begin,end,results.get(i)));
+
+       thread[i].start();
+       begin = begin + section;
+       end = end + section;       
+    }
+
+     for(int j = 0; i < numThreads; ++j){
+        try {
+            thread[i].join();
+            } catch (InterruptedException e) {
+            e.printStackTrace();
+            }
+       }
+
+     for(int k = 0; k < result.size(); k++ ){
+      if(result.get(k).answer){
+         return true;
+      }
+     }
     return false;
   }
 
+  @SuppressWarnings("unused")
   public void run() {
-
+  search(this.target,this.list,this.start,this.end);
+  }
+  
+  private void search(T target, ArrayList<T> list, int start, int end){
+   for(int j = start; j < end; j++ ){
+      if(list.get(j).equals(target)){
+         answer = true;
+       }
+   }
   }
 
   private class Answer {
-    private boolean answer = false;
-
-    public boolean getAnswer() {
-      return answer;
-    }
-
-    // This has to be synchronized to ensure that no two threads modify
-    // this at the same time, possibly causing race conditions.
-    public synchronized void setAnswer(boolean newAnswer) {
-      answer = newAnswer;
-    }
+    public boolean answer = false;
   }
 
 }
